be42a6544514113c63b052c36240c3d949868d2c
Bleaktorium <martiiryven@gmail.com>
2025-08-23 09:49:08 +0100

    :)

---
+++ src/main/java/net/bleaktorium/helens_armory/Helens_Armory.java
@@ -0,0 +1,61 @@
+package net.bleaktorium.helens_armory;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+
+@Mod(Helens_Armory.MOD_ID)
+public class Helens_Armory {
+    public static final String MOD_ID = "helens_armory";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    // CONSTRUCTOR
+    public Helens_Armory(IEventBus modEventBus, ModContainer modContainer){
+
+        modEventBus.addListener(this::commonSetup);
+
+        NeoForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+
+    }
+
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+    }
+
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+
+    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            
+        }
+    }
+}
