b97e0ef7b05e5e06712183df79295b0767041592
Bleaktorium <martiiryven@gmail.com>
2025-08-23 13:16:02 +0100

    started item deferred register

---
+++ src/main/java/net/bleaktorium/helens_armory/item/ModItems.java
@@ -0,0 +1,9 @@
+package net.bleaktorium.helens_armory.item;
+
+import net.bleaktorium.helens_armory.Helens_Armory;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Helens_Armory.MOD_ID);
+
+}
