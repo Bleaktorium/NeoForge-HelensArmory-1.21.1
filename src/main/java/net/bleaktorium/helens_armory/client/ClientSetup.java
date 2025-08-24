cf85b02be267ac55350c144bea1db3ce596ca20e
Bleaktorium <martiiryven@gmail.com>
2025-08-24 18:43:33 +0100

    added geckolib and curios

--- src/main/java/net/bleaktorium/helens_armory/client/ClientSetup.java
+++ src/main/java/net/bleaktorium/helens_armory/client/ClientSetup.java
@@ -4,12 +4,13 @@ import net.bleaktorium.helens_armory.client.render.TestBackCurioGeoRenderer;
 import net.bleaktorium.helens_armory.item.ModItems;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import top.theillusivec4.curios.api.client.CuriosRendererRegistry;
 import net.neoforged.fml.common.Mod;
 
 
-@Mod.EventBusSubscriber(modid = "helens_armory", bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@EventBusSubscriber(modid = "helens_armory", bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientSetup {
     @SubscribeEvent
     public static void onClientSetup(final FMLClientSetupEvent e) {
--- src/main/java/net/bleaktorium/helens_armory/client/render/TestBackCurioGeoRenderer.java
+++ src/main/java/net/bleaktorium/helens_armory/client/render/TestBackCurioGeoRenderer.java
@@ -1,43 +1,48 @@
 package net.bleaktorium.helens_armory.client.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.bleaktorium.helens_armory.client.model.TestBackCurioModel;
-import net.bleaktorium.helens_armory.item.TestBackCurioItem;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
-import software.bernie.geckolib.animatable.GeoAnimatable;
-import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.renderer.GeoItemRenderer;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.client.ICurioRenderer;
+import net.bleaktorium.helens_armory.item.TestBackCurioItem;
+import net.bleaktorium.helens_armory.client.model.TestBackCurioModel;
 
-public class TestBackCurioGeoRenderer extends GeoItemRenderer<TestBackCurioItem> implements ICurioRenderer {
-    public TestBackCurioGeoRenderer() {
-        super(new TestBackCurioModel());
-    }
+public class TestBackCurioGeoRenderer extends GeoItemRenderer<TestBackCurioItem>
+        implements ICurioRenderer {
 
+    public TestBackCurioGeoRenderer() { super(new TestBackCurioModel()); }
 
     @Override
     public <T extends LivingEntity, M extends EntityModel<T>> void render(
-            ItemStack stack, SlotContext slotContext, PoseStack matrixStack,
-            RenderLayerParent<T, M> renderLayerParent, MultiBufferSource renderTypeBuffer,
-            int light, float limbSwing, float limbSwingAmount, float partialTicks,
-            float ageInTicks, float netHeadYaw, float headPitch) {
+            ItemStack stack,
+            SlotContext slotCtx,
+            PoseStack pose,
+            RenderLayerParent<T, M> parent,
+            MultiBufferSource buffers,
+            int light,
+            float limbSwing, float limbSwingAmount,
+            float partialTick, float ageInTicks,
+            float netHeadYaw, float headPitch) {
+
         pose.pushPose();
 
-        if (renderLayerParent.getModel() instanceof HumanoidModel<?> hum) {
-            top.theillusivec4.curios.api.client.ICurioRenderer.followBodyRotations(ctx.entity(), hum);
+        if (parent.getModel() instanceof HumanoidModel<?> hum) {
+            ICurioRenderer.followBodyRotations(slotCtx.entity(), hum);
             hum.body.translateAndRotate(pose);
         }
 
+        // adjust to taste
         pose.translate(0.0D, 0.15D, 0.25D);
-        this.render(stack, net.minecraft.world.item.ItemDisplayContext.NONE, pose, buffers, light,
-                net.minecraft.client.renderer.texture.OverlayTexture.NO_OVERLAY);
-        pose.popPose();
 
+        this.render(stack, ItemDisplayContext.NONE, pose, buffers, light, OverlayTexture.NO_OVERLAY);
+        pose.popPose();
     }
 }
